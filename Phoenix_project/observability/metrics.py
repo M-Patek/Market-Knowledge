63252eeb388f4ff5759b6c5ac92ca8ac4c177309
M-I3reak <2191595231@qq.com>
2025-11-01 21:21:11 +0800

    Delete Phoenix_project/observability directory

--- Phoenix_project/observability/metrics.py
+++
@@ -1,7 +0,0 @@
-# Phoenix_project/observability/metrics.py
-from prometheus_client import Counter, Histogram
-
-REQ_TOTAL = Counter("http_requests_total", "Total HTTP requests", ["path"])
-L1_LAT = Histogram("l1_agent_latency_seconds", "Latency of L1 agents")
-FUSION_CONFLICTS = Counter("fusion_conflict_count", "Number of L2 conflicts resolved")
-UNCERTAINTY = Histogram("uncertainty_score", "Distribution of uncertainty scores")
